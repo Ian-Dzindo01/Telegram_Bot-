5fac212849691bdc1208b9aa5da7b668e80143ce
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-01-20 16:55:13 +0100

    First barely working model.

--- training.py
+++ training.py
@@ -6,9 +6,9 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
+import tensorflow.keras.models
+import tensorflow.keras.layers
+import tensorflow.keras.optimizers
 
 nltk.download('wordnet')
 
@@ -44,8 +44,7 @@ for document in documents:
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
-        bag.append(1) if word in word_patterns else bag.append(0):
-
+        bag.append(1) if word in word_patterns else bag.append(0)
 
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
@@ -54,11 +53,19 @@ for document in documents:
 random.shuffle(training)
 training = np.array(training)
 
-train_x = list(training[:, 0)]
-train_y = list(training[:, 1)]
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
 
-model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))                                 # prevent overfitting
-model.add(Dense(64, activation='relu'))                 # 64 neurons
-model.add(Dropout)
+model = tensorflow.keras.Sequential()
+model.add(tensorflow.keras.layers.Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(tensorflow.keras.layers.Dropout(0.5))                                 # prevent overfitting
+model.add(tensorflow.keras.layers.Dense(64, activation='relu'))                 # 64 neurons
+model.add(tensorflow.keras.layers.Dropout(0.5))
+model.add(tensorflow.keras.layers.Dense(len(train_y[0]), activation='softmax'))
+
+sgd = tensorflow.keras.optimizers.SGD(decay=1e-6, momentum=0.9, nesterov=True)                  # decay parameter reduces the learning rate over time
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_simple_model.model')
+print("Finished")
